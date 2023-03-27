34de3057756d9c15863828cbf90e89061746cb50
Nathan H - Lab 303 <114543371+SAEStudentNathanHoward@users.noreply.github.com>
2023-03-27 15:40:10 +1100

    Added new script that plays debug log

---
+++ Assets/Scripts/NewScriptNathanH.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewScriptNathanH : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Welcome to the new project.");
+        Debug.Log("This was left by Nathan H.");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
